b04c1979f0cedf35f93003a35764de0a4559d1b2
haeunNoh06 <okhagrace06@naver.com>
2024-08-13 09:58:14 +0900

    제네릭 실습 2

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -14,6 +14,18 @@ namespace CSAdv
             Value = value;
         }
     }
+
+    class Needed<T, U> // Generic
+    {
+        public T Value1;
+        public U Value2;
+        public Needed(T value1, U value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
